188610c9af71500e1459b861b1b5a8f203116434
Thiyagu2006 <ghillieditz@gmail.com>
2025-07-27 09:58:49 +0530

    Initial commit

---
+++ calculator/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from calculator import views
+urlpatterns = [
+    path('thiyagu/mysimplecalulator/app', views.calculator, name= 'calculator')
+]
---
+++ calculator/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def calculator(request):
+    result = None
+    if request.method == 'POST':
+        a = int(request.POST.get('a'))
+        b = int(request.POST.get('b'))
+        operator = request.POST.get('operator')
+
+        if operator == 'Addition':
+            result = a + b
+        elif operator == 'Subtraction':
+            result = a - b
+        elif operator == 'Multiplication':
+            result = a * b
+        elif operator == 'Division':
+            result = a / b
+
+    context = {'result': result}
+    return render(request, 'calculator.html', context)
