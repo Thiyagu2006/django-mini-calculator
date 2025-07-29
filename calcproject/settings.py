5ac731432006b65f8e03e5a53534b4e2cf6726fe
Thiyagarajan <ghilliedtiz@gmail.com>
2025-07-29 10:52:35 +0530

    Update settings.py

--- calcproject/settings.py
+++ calcproject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'calcproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, 'mysimplecalulator', "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
