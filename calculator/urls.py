a97e3175cbdc68c80300e97aa5595999f459ea4f
Thiyagarajan <ghilliedtiz@gmail.com>
2025-07-29 11:08:07 +0530

    Update urls.py

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,5 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from calculator import views
+from mysimplecalulator import views
+
 urlpatterns = [
-    path('thiyagu/mysimplecalulator/app', views.calculator, name= 'calculator')
+    path('', views.calculator, name='home'),  # default home page
+    path('admin/', admin.site.urls),
+    path('thiyagu/mysimplecalulator/app', views.calculator, name='calculator'),
 ]
