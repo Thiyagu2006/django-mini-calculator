702055bbebb2bbcb146156a9d67caf54e9ae75d4
Thiyagarajan <ghilliedtiz@gmail.com>
2025-07-29 11:33:49 +0530

    Update urls.py

--- calcproject/urls.py
+++ calcproject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mysimplecalulator import views
+from mysimplecalculator import views
 
 urlpatterns = [
     path('', views.calculator, name='home'), 
