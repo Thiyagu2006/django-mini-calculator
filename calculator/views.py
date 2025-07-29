d5b1e9cd87aa31604eef69b67577e380fe321f08
Thiyagarajan <ghilliedtiz@gmail.com>
2025-07-29 10:17:03 +0530

    Update views.py

--- calculator/views.py
+++ calculator/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+def home(request):
+    return HttpResponse("Welcome to my Django Calculator!")
 def calculator(request):
     result = None
     if request.method == 'POST':
