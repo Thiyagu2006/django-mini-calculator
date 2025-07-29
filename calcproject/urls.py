d9baea8742da6481484cb38184ef0a0566ddeac9
Thiyagarajan <ghilliedtiz@gmail.com>
2025-07-29 10:58:05 +0530

    Update urls.py

--- calcproject/urls.py
+++ calcproject/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from mysimplecalulator import views
 
 urlpatterns = [
+    path('', views.calculator, name='home'),  # Homepage ku route
     path('admin/', admin.site.urls),
     path('thiyagu/mysimplecalulator/app', views.calculator, name='calculator'),
+
 ]
 
 
